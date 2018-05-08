3d325779e1103d00685aaebf48063d25a59f87f4
yasushiIS <ishizuka@iszk.net>
2018-05-08 19:38:58 +0900

    add param_pharmacy.js

---
+++ js/param_pharmacy.js
@@ -0,0 +1,30 @@
+//  エリア別パラメータ
+//  奈良公園エリア用
+
+//  背景地図：初期表示の中心の緯度・経度
+DEF_LAT=34.6850; 
+DEF_LON=135.8380; 
+
+//  背景地図：初期表示のズームレベル（縮尺）
+DEF_ZOOM=16;
+
+//  イラスト地図ファイル
+DEF_IMAGEMAP = './map/narapark.png';
+
+//  イラスト地図表示位置の緯度・経度（左上）
+IMG_LAT1=34.6903;
+IMG_LON1=135.82725; 
+
+//  イラスト地図表示位置の緯度・経度（右下）
+IMG_LAT2=34.6816;
+IMG_LON2=135.84599;
+
+//  おすすめルート
+GEOJSON_ROUTE = './geojson/test_rt.geojson';
+GEOJSON_POINT = './geojson/test_po.geojson';
+
+//  多目的トイレ情報
+GEOJSON_TOILET = './geojson/test_toilet.geojson';
+
+//  通行情報
+GEOJSON_PASS  = './geojson/test_pass.geojson';
