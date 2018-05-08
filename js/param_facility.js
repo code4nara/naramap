a084d2c54f43b46e6e275e3b02b651c126596dc5
Yasushi ISHIZUKA <ishizuka@zukatech.com>
2018-05-08 19:33:45 +0900

    add parameter

---
+++ js/param_facility.js
@@ -0,0 +1,20 @@
+//  エリア別パラメータ
+//  施設情報
+
+//  背景地図：初期表示の中心の緯度・経度
+DEF_LAT=34.6850; 
+DEF_LON=135.8380; 
+
+//  背景地図：初期表示のズームレベル（縮尺）
+DEF_ZOOM=16;
+
+//  施設
+GEOJSON_TOILET   = './geojson/toilet.geojson';
+
+//  施設
+GEOJSON_FACILITY = './geojson/facility.geojson';
+
+//  薬局
+GEOJSON_PHARMACY  = './geojson/pharmacy.geojson';
+
+DEFALT_DATA = "FACILITY";
