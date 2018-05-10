dcc15332255cac40b4c455ae5fcce0800184ae47
Yasushi ISHIZUKA <ishizuka@zukatech.com>
2018-05-10 12:40:15 +0900

    add param_public_facility

---
+++ js/param_public_facility.js
@@ -0,0 +1,24 @@
+//  表示種別パラメータ
+//  禁煙施設情報
+
+//  背景地図：初期表示の中心の緯度・経度
+DEF_LAT=34.6900; 
+DEF_LON=135.8200; 
+
+//  背景地図：初期表示のズームレベル（縮尺）
+DEF_ZOOM=13;
+
+//  フォルト
+DEFALT_DATA = "PUBLIC_FACILITY";
+
+//  多目的トイレ
+GEOJSON_TOILET   = './geojson/toilet.geojson';
+
+//  禁煙施設
+GEOJSON_FACILITY = './geojson/facility.geojson';
+
+//  禁煙薬局
+GEOJSON_PHARMACY  = './geojson/pharmacy.geojson';
+
+//  公共施設
+GEOJSON_PUBLIC_FACILITY = './geojson/public_facility.geojson';
